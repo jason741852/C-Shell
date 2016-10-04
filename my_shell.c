3ce260006490de08483c01d095d9ea266219a3f6
ksc19 <ksc19@csil-cpu2.cs.surrey.sfu.ca>
2016-10-04 11:33:35 -0700

    implemented exit()

--- my_shell.c
+++ my_shell.c
@@ -1,46 +1,116 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <string.h>
+#include <stdbool.h>
+
+#define tokenDelim " \n "
+#define bufferSize 100
+
+char* readCmd();
+char* readCmd();
+char** tokenizer(char*);
+bool exitCheck(char**);
+//bool execute(char**);
+
 
 int main(void){
-
-
-  printf("Please enter a command:\n");
-
-  char* line = NULL;
-  ssize_t bufferSize = 0;
-  getline(&line, &bufferSize, stdin);
-
-
-  char *tokenizer(char* line, ssize_t bufferSize){
-    char* tokens[bufferSize];
+  bool terminate = false;
+  while(!terminate){
+    char* cmd;
+    char** cmdToken;
     int i = 0;
-    tokens[i] = strtok(line," ");
+    pid_t pid;
 
-    while(tokens[i]!=NULL)
-    {
-      tokens[++i] = strtok(NULL," ");
+
+    cmd = readCmd();
+    cmdToken = tokenizer(cmd);
+    //  for(i = 0; i<3; i++){
+    //    printf("%s | ", cmdToken[i]);
+    //  }
+
+    if(exitCheck(cmdToken)==true){
+      printf("exiting shell..\n");
+      terminate = true;
+      exit(0);
     }
 
+    /////////////////////////////////////////////
+    /*forking if command not calling build-in's*/
+    /////////////////////////////////////////////
 
-  //printf("%s",line);
-
-
-
+    pid = fork();
+    if(pid<0){
+      perror("error");
+      //exit(1);
+    }
+    else if(pid==0){
+      if(execvp(cmdToken[0],cmdToken)==-1){
+        perror("command not found");
+      }
+    }
+    else{
+      wait();
+      printf("parent exiting\n");
+      //exit(0);
+    }
+}
   return 0;
 }
-//
-//
-// char *tokenizer(char* line, ssize_t bufferSize){
-//   char* tokens[bufferSize];
-//   int i = 0;
-//   tokens[i] = strtok(line," ");
-//
-//   while(tokens[i]!=NULL)
-//   {
-//     tokens[++i] = strtok(NULL," ");
-//   }
-//
-//   return tokens;
+
+
+/// Print out a prompt then read a command from user
+char* readCmd(){
+  printf("Please enter a command:\n");
+  char* line = NULL;
+  ssize_t buf = 0;
+  getline(&line, &buf, stdin);
+  return line;
 }
+
+/// Split command arguement into tokens
+char** tokenizer(char* line){
+  char** tokens = malloc(bufferSize);
+  char* token;
+  int i = 0; //array position counter
+
+  tokens[i] = strtok(line,tokenDelim);
+
+  while(tokens[i]!=NULL)
+  {
+    tokens[++i] = strtok(NULL,tokenDelim);
+  }
+  return tokens;
+}
+
+// Check for build-in system calls
+int buildinCheck(char** cmdTok){
+  if(strcmp("cd", cmdTok[0])==0){
+    return 1;
+  }
+  else if(strcmp("exit", cmdTok[0])==0){
+    //printf("true \n");
+    return 2;
+  }
+}
+
+
+
+
+
+
+
+
+
+/// Check for "exit" command, if so exit
+ bool exitCheck(char** cmdTok){
+   if(strcmp("exit", cmdTok[0])==0){
+     //printf("true \n");
+     return true;
+   }
+   else{
+     //printf("false \n");
+     return false;
+   }
+ }
