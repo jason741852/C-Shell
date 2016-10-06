2c45410a3a6f2cda7154114a42fedffe5e23b6df
ksc19 <ksc19@asb9700u-a06.csil.sfu.ca>
2016-10-05 17:06:57 -0700

    Implemented Pipe symbal postion array

--- my_shell.c
+++ my_shell.c
@@ -10,30 +10,42 @@
 
 char* readCmd();
 char* readCmd();
-char** tokenizer(char*);
-bool exitCheck(char**);
-bool cdCheck(char**);
-int cmdCount(char**);
-
+char** tokenizer(char*);// Split commands into tokens
+bool exitCheck(char**);// Check for exit()
+bool cdCheck(char**);// Check for cd
+int cmdCount(char**);// Return the number of commands (for piping)
+int* pipeSymbolPostions(char**, int);// Return an array of position of pipe symbol
+void pipeRoutine(char**, int*,int, int , int);
 
 
 int main(void){
+  // only two pipes is need at the same time,
+  // regardless of the number of commands(???)
+  int fdP[2];
+  int fdC[2];
+
+
+
   bool terminate = false;
   while(!terminate){
     char* cmd;
     char* cwd;
     char** cmdToken;
+    char** cmdArrays;
     int i = 0;
     int cmd_counter = 0; // count the number of pipes needed
     pid_t pid;
 
-
     cmd = readCmd();
     cmdToken = tokenizer(cmd);
-    cmd_counter = cmdCount(cmdToken);
-    //  for(i = 0; i<3; i++){
-    //    printf("%s | ", cmdToken[i]);
+    int* pipePositions =  pipeSymbolPostions(cmdToken, cmdCount(cmdToken)-1);
+    // printf("Pipe Symbol Position: ");
+    //  for(i = 0; i<cmdCount(cmdToken)-1; i++){
+    //    printf("%d , ", pipePositions[i]);
     //  }
+    //  i++;
+    // printf("\n what's in next: %d ", pipePositions[i]);
+    printf("\n");
 
     if(exitCheck(cmdToken)==true){
       printf("exiting shell..\n");
@@ -56,30 +68,33 @@ int main(void){
       }
     }
     else{
-    /////////////////////////////////////////////
-    /*forking if command not calling build-in's*/
-    /////////////////////////////////////////////
-    pid = fork();
-    printf("pid = %d\n", pid);
-    if(pid<0){
-      perror("error");
-      exit(1);
-    }
-    else if(pid==0){
-      printf("------in child------\n");
-      if(execvp(cmdToken[0],cmdToken)<0){
-        perror("error: execution failed");
+      pipe(fdP);// Child -> Parent
+      pipe(fdC);// Parent -> Child
+
+      /////////////////////////////////////////////
+      /*forking if command not calling build-in's*/
+      /////////////////////////////////////////////
+      pid = fork();
+      printf("pid = %d\n", pid);
+      if(pid<0){
+        perror("error");
+        exit(1);
+      }
+      else if(pid==0){
+        printf("------in child------\n");
+        if(execvp(cmdToken[0],cmdToken)<0){
+          perror("error: execution failed");
+        }
+      }
+      else{
+        printf("------parent waiting------\n");
+        wait();
+        printf("------parent finished waiting------\n");
+        printf("parent exiting\n");
+        exit(0);
       }
     }
-    else{
-      printf("------parent waiting------\n");
-      wait();
-      printf("------parent finished waiting------\n");
-      printf("parent exiting\n");
-      //exit(0);
-    }
   }
-}
   return 0;
 }
 
@@ -105,6 +120,7 @@ char** tokenizer(char* line){
   {
     tokens[++i] = strtok(NULL,tokenDelim);
   }
+  tokens[i] = NULL;
   return tokens;
 }
 
@@ -134,14 +150,46 @@ char** tokenizer(char* line){
  }
 
  int cmdCount(char** cmdTok){
-   int n = sizeof(cmdTok)/sizeof(cmdTok[0]);
+   int n = 0;//tokens counter
+   while(cmdTok[n]!=NULL){
+     n++;
+   }
    int cmd_counter=1;
+
    int i = 0;//loop counter
    for(i; i<n; i++){
-     if(strcmp("|", cmdTok[i])==0){
+     //printf("cmp result: %d  \n", strcmp("|", cmdTok[i]));
+     if((strcmp("|", cmdTok[i]))==0){
        cmd_counter++;
      }
    }
-   printf("Number of commands: %d\n", cmd_counter);
+   //printf("Number of commands: %d\n", cmd_counter);
    return cmd_counter;
+ }
+
+ int* pipeSymbolPostions(char** cmdTok, int cmdsTotal){
+   int i = 0;
+   int j = 0;
+   int* pipePos = malloc((cmdsTotal+1)*sizeof(int));
+   //printf("Pipe Symbol Position: ");
+   while(cmdTok[i]!=NULL){
+     if ((strcmp("|", cmdTok[i]))==0){
+       pipePos[j] = i;
+       //printf("Pos= %d , ", i);
+       j++;
+     }
+     i++;
+   }
+   //printf("\n");
+   return pipePos;
+ }
+
+// Need to make a function to concatenate strings for execvp using curPos and nextPos
+ void pipeRoutine(char** cmdTok, int* pos, int curPos, int nextPos, int input_fd){
+   // Base case: last command
+   if(pos[i+1] == 0){
+     dup2(input_fd, STDOUT_FILENO);
+
+
+   }
  }
